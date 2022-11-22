8320d1f9b52d561dd26ae83723de2b53722d7db5
RobJ <robjjohnsenwork@gmail.com>
2022-11-22 13:49:13 +0700

    Building out the Log Component - private. Added select ability and the start of the debit template to prepare for update

--- src/Components/Log/Log-private-component.jsx
+++ src/Components/Log/Log-private-component.jsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 //
+const clientTestArray = [
+  { name: "Rob Johnsen", id: "1" },
+  { name: "Mai Jacobs", id: "2" },
+  { name: "Tom Cruise", id: "3" },
+  { name: "Joe Soap", id: "4" },
+  { name: "Tim Burton", id: "5" },
+  { name: "Mike Pooperson", id: "6" },
+  { name: "Jordy Johnson", id: "7" },
+  { name: "Bill Morrison", id: "8" },
+];
+const privateClientDebitTemplate = {
+  clientToDebit: "",
+  debitInfo: {},
+};
 //
-
 function LogPrivate(params) {
-  return <div>Hey im the Private component!</div>;
+  //
+  const [privateClientDebit, setPrivateClientDebit] = useState(
+    privateClientDebitTemplate
+  );
+  //
+  const investigate = (e) => {
+    e.preventDefault();
+    // Everytime the select option changes...
+    // Find id of client that is chosen
+    const clientChosenId = e.target.value;
+    // Return the object of that client using the id : if needed
+    const findClient = clientTestArray.find(
+      (client) => client.id === clientChosenId
+    );
+    setPrivateClientDebit({
+      ...privateClientDebit,
+      clientToDebit: clientChosenId,
+    });
+  };
+  //
+  const checkSituation = (e) => {
+    e.preventDefault();
+    console.log(privateClientDebit);
+  };
+  //
+  return (
+    <div className="flex gap-2 p-2">
+      <div className="w-1/2 bg-gray-200 flex flex-col gap-2">
+        <h2>Client:</h2>
+        <button type="button" onClick={checkSituation} className="bg-white">
+          Check situation
+        </button>
+      </div>
+
+      <div className="w-1/2 bg-gray-200">
+        <select
+          onChange={investigate}
+          id="clients"
+          className="w-full text-center p-1"
+        >
+          {clientTestArray.map((client) => {
+            return (
+              <option key={client.id} value={client.id}>
+                {client.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default LogPrivate;
