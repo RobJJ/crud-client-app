6ff4356ca654fa522424594fbe2af6ab08ae20ee
RobJ <robjjohnsenwork@gmail.com>
2022-11-22 11:39:29 +0700

    Implemented first workings of Log Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-//
+// Components
 import ReceiptList from "./Components/Receipts/Receipt-parent-component";
 import AddClient from "./Components/AddClient/AddClient-component";
 import Navigation from "./Components/Navigation/Navigation-parent-component";
+import Log from "./Components/Log/Log-parent-component";
+//
 
 function App() {
   return (
     <div className="w-screen h-screen bg-blue-100 flex justify-center items-center">
-      <Navigation />
+      <Log />
     </div>
   );
 }
--- src/Components/Log/Log-parent-component.jsx
+++ src/Components/Log/Log-parent-component.jsx
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 //
+const privateInfo = {
+  name: "private",
+  info: "private stuff here",
+};
+const classInfo = {
+  name: "class",
+  info: "class stuff here",
+};
 
-function Log(params) {}
+function Log(params) {
+  //
+  const [currentLogPage, setCurrentLogPage] = useState({});
+
+  const tray = useRef();
+  //
+  const handleLogClick = (e) => {
+    e.preventDefault();
+    const pageRequested = e.target.dataset.tag;
+    // check what is requested based on click
+    // set the currentPage to the correct obj.
+    pageRequested === "log-private"
+      ? setCurrentLogPage(privateInfo)
+      : setCurrentLogPage(classInfo);
+    //
+    if (tray.current.classList.contains("hidden")) {
+      tray.current.classList.remove("hidden");
+    } else {
+      tray.current.classList.add("hidden");
+    }
+  };
+  //
+  return (
+    <div className="w-2/3 h-2/3 bg-green-100 p-2">
+      {/* Parent container that expands onClick. No height set */}
+      <div className="w-full bg-green-200 flex flex-col">
+        {/* Top level bar display to handle clicks and guidance */}
+        <div className="w-full h-12 flex text-center items-center gap-2">
+          <div
+            data-tag="log-private"
+            onClick={handleLogClick}
+            className="w-1/3 bg-green-500"
+          >
+            PRIVATE
+          </div>
+          <div className="w-1/3 bg-green-500">LOG</div>
+          <div
+            data-tag="log-class"
+            onClick={handleLogClick}
+            className="w-1/3 bg-green-500"
+          >
+            CLASS
+          </div>
+        </div>
+        {/* The expandable section of the div.. The contents depends on the click */}
+        <div ref={tray} className="hidden ">
+          <h2>{currentLogPage.name}</h2>
+          <p>{currentLogPage.info}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Log;
