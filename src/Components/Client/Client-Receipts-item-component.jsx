45d5248f66e9e4c86529690cb07050376d54cb5c
RobJ <robjjohnsenwork@gmail.com>
2022-12-04 16:06:46 +0700

    Added note drop down effect on receipt items

--- src/Components/Client/Client-Receipts-item-component.jsx
+++ src/Components/Client/Client-Receipts-item-component.jsx
@@ -1,17 +1,31 @@
+import { useRef } from "react";
 import React from "react";
 //
 //
 function ReceiptItem({ receipt }) {
-  const { date, amount, sessions } = receipt;
+  const { date, amount, sessions, note } = receipt;
+  const receiptNote = useRef();
+  const toggleNote = () => {
+    if (receiptNote.current.classList.contains("hidden")) {
+      receiptNote.current.classList.remove("hidden");
+    } else {
+      receiptNote.current.classList.add("hidden");
+    }
+  };
   return (
     <div
-      className={`w-full flex gap-2 ${
+      className={`flex flex-col ${
         amount ? "bg-blue-200" : "bg-yellow-200"
-      } text-center text-xl p-2 rounded-xl`}
+      } rounded-xl text-center`}
     >
-      <span className="w-1/3 ">{date}</span>
-      <span className="w-1/3 ">{amount ? amount : "N/A"}</span>
-      <span className="w-1/3 ">{sessions}</span>
+      <div onClick={toggleNote} className={`w-full flex gap-2   text-xl p-2 `}>
+        <span className="w-1/3 ">{date}</span>
+        <span className="w-1/3 ">{amount ? amount : "N/A"}</span>
+        <span className="w-1/3 ">{sessions}</span>
+      </div>
+      <span ref={receiptNote} className="hidden border-t-2 border-white p-1">
+        {note}
+      </span>
     </div>
   );
 }
