12f59439465943e32e91454070b5cd043d495283
RobJ <robjjohnsenwork@gmail.com>
2022-12-05 14:54:22 +0700

    client page transaction component implemented. Debits and credits to both state and DB

--- src/Components/Client/Client-Transaction-component.jsx
+++ src/Components/Client/Client-Transaction-component.jsx
@@ -1,52 +1,177 @@
 import React, { useState, useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
+import FormRow from "../FormRow/FormRow-component";
+import { useGlobalContext } from "../../Context-Reducer/Context";
+
 //
 //
-const transactionTemplate = {
-  type: "",
-  date: "",
-  amount: "",
-  sessions: 0,
-  notes: "",
-  id: "",
-};
 //
 function ClientTransaction(params) {
+  // Transaction type is "" false on load in
+  const [transactionType, setTransactionType] = useState("");
   //
-  const [transaction, setTransaction] = useState(transactionTemplate);
-  const formRef = useRef();
+  const {
+    clients,
+    handleDebit,
+    debitClient,
+    setDebitClient,
+    creditClient,
+    setCreditClient,
+    handleCredit,
+  } = useGlobalContext();
   //
-  const handleTransactionChoice = (e) => {
-    // set the transaction type
-    setTransaction({ ...transaction, type: e.target.dataset.tag });
-    // hide amount if credit is picked
-
-    //  make current button active with adding a style of some sort
-    e.target.classList.add("bg-slate-300");
-    // show the form to user
-    formRef.current.classList.remove("opacity-25");
-  };
   //
-  const handleTransactionSubmit = (e) => {
-    e.preventDefault();
-    // setTransaction((prev) => {
-    //   return {
-    //     ...prev,
-    //     id: uuidv4(),
-    //   };
-    // });
-    console.log(transaction);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    // Dynamically looks for name and value pairing from input.
+    // This methods reduces clutter in the form
+    if (transactionType === "debit") {
+      setDebitClient({
+        ...debitClient,
+        [name]: value,
+      });
+    } else if (transactionType === "credit") {
+      setCreditClient({
+        ...creditClient,
+        [name]: value,
+      });
+    }
   };
   //
   return (
     <div className="bg-red-200 w-full h-full p-5 flex justify-center">
-      <form className="bg-red-300 max-w-md w-full h-full flex flex-col gap-2 p-2 ">
+      <form
+        onSubmit={transactionType === "debit" ? handleDebit : handleCredit}
+        className="bg-red-300 max-w-md w-full h-full flex flex-col gap-2 p-2 "
+      >
         <section className="bg-yellow-200 text-center flex p-2 gap-1 text-xl">
-          <div className="w-1/2 bg-white">Debit</div>
-          <div className="w-1/2 bg-white">Credit</div>
+          <div
+            className={`w-1/2 ${
+              transactionType === "debit" ? "bg-blue-300" : "bg-white"
+            }`}
+            onClick={() => setTransactionType("debit")}
+          >
+            Debit
+          </div>
+          <div
+            className={`w-1/2 ${
+              transactionType === "credit" ? "bg-blue-300" : "bg-white"
+            }`}
+            onClick={() => setTransactionType("credit")}
+          >
+            Credit
+          </div>
         </section>
-        <section className="bg-yellow-200">Inputs</section>
-        <section className="bg-yellow-200">Submit</section>
+        {/* DEBIT SECTION */}
+        {transactionType === "debit" && (
+          <section className="bg-yellow-200 p-1 flex flex-col gap-1">
+            {/* select client to debit / credit */}
+            <div className="flex flex-col bg-blue-100 p-2 gap-1">
+              <label className=" pl-2">Client</label>
+              <select
+                required
+                id="clientList"
+                value={debitClient.name}
+                name="name"
+                onChange={handleInputChange}
+                className="w-full text-center p-1 rounded-lg"
+              >
+                <option value="Select client..." hidden>
+                  Select client...
+                </option>
+                {clients.map((client) => {
+                  return (
+                    <option key={client.uid} value={client.name}>
+                      {client.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            {/* date */}
+
+            <FormRow
+              type="date"
+              name="date"
+              labelText="Date"
+              value={debitClient.date}
+              handleChange={handleInputChange}
+            />
+            <FormRow
+              type="text"
+              name="amount"
+              labelText="Amount"
+              value={debitClient.amount}
+              handleChange={handleInputChange}
+            />
+            <FormRow
+              type="text"
+              name="sessions"
+              labelText="Sessions"
+              value={debitClient.sessions}
+              handleChange={handleInputChange}
+            />
+            <FormRow
+              type="text"
+              name="note"
+              labelText="Note"
+              value={debitClient.note}
+              handleChange={handleInputChange}
+            />
+          </section>
+        )}
+        {/* CREDIT SECTION */}
+        {transactionType === "credit" && (
+          <section className="bg-yellow-200 p-1 flex flex-col gap-1">
+            {/* select client to debit / credit */}
+            <div className="flex flex-col bg-blue-100 p-2 gap-1">
+              <label className=" pl-2">Client</label>
+              <select
+                required
+                id="clientList"
+                value={creditClient.name}
+                name="name"
+                onChange={handleInputChange}
+                className="w-full text-center p-1 rounded-lg"
+              >
+                <option value="Select client..." hidden>
+                  Select client...
+                </option>
+                {clients.map((client) => {
+                  return (
+                    <option key={client.uid} value={client.name}>
+                      {client.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            {/* date */}
+            <FormRow
+              type="date"
+              name="date"
+              labelText="Date"
+              value={creditClient.date}
+              handleChange={handleInputChange}
+            />
+            <FormRow
+              type="text"
+              name="note"
+              labelText="Note"
+              value={creditClient.note}
+              handleChange={handleInputChange}
+            />
+          </section>
+        )}
+        {/* submit section */}
+        {transactionType && (
+          <section className="bg-yellow-200 p-1">
+            <div className="bg-blue-100 text-center ">
+              <button type="submit" className="bg-white p-1 rounded-xl">
+                Submit
+              </button>
+            </div>
+          </section>
+        )}
       </form>
     </div>
   );
@@ -56,3 +181,11 @@ export default ClientTransaction;
 
 // give the form width a max size.. and then make it cover full space if able
 // with the max width set.. it will only be that big and then shrink as screen size decreases... set max width to a size... but also set w-full
+
+// DEBIT
+// name: "",
+//   date: "",
+//   amount: "",
+//   sessions: "",
+//   note: "",
+//   uid: "",
---
+++ src/Components/FormRow/FormRow-component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//
+function FormRow({ type, name, value, handleChange, labelText }) {
+  return (
+    <div className="flex flex-col p-2 gap-1 bg-blue-100">
+      <label className="pl-2">{labelText}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className="p-1 pl-2 rounded-lg"
+      />
+    </div>
+  );
+}
+//
+export default FormRow;
