c811ae02d98aff1ecd521666fbfa0c70a6ce2a97
RobJ <robjjohnsenwork@gmail.com>
2022-12-07 17:05:13 +0700

    Styling tweeks for Navbar. Implemented loading Spinner

--- src/Components/Navbar/Navbar-component.jsx
+++ src/Components/Navbar/Navbar-component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../Context-Reducer/Context";
 import { FaHome } from "react-icons/fa";
+import { ReactComponent as Spinner } from "../LoadingIcon/Bars-0.8s-234px.svg";
+
 //
 import { signOutUser } from "../../Firebase/firebase-auth";
 //
@@ -21,10 +23,10 @@ function Navbar(params) {
   };
   //
   return (
-    <div className="bg-blue-800 grid grid-cols-3 p-5 text-center font-mainNavBar tracking-widest">
+    <div className="bg-blue-800 flex px-5 py-2 text-center font-mainNavBar tracking-widest">
       <Link
         onClick={handleSignOut}
-        className="bg-blue-800 underline text-2xl text-white"
+        className="bg-blue-800 w-1/3 p-2 underline text-2xl text-white"
       >
         SIGN OUT
       </Link>
@@ -32,16 +34,18 @@ function Navbar(params) {
       <Link
         to="/home"
         onClick={handleHomeNavigation}
-        className="bg-blue-800 underline text-2xl flex items-center justify-center gap-2 text-white"
+        className="bg-blue-800 w-1/3 p-2 underline text-2xl flex items-center justify-center gap-2 text-white"
       >
         <FaHome />
         <div>HOME</div>
       </Link>
 
-      <div className="bg-blue-800 text-white underline text-2xl">
-        {userInfo.displayName
-          ? `${userInfo.displayName.toUpperCase()}`
-          : "Loading spinner"}
+      <div className="bg-blue-800 w-1/3 p-2 text-white underline text-2xl flex justify-center items-center">
+        {userInfo.displayName ? (
+          `${userInfo.displayName.toUpperCase()}`
+        ) : (
+          <Spinner className="w-20 p-0 m-0 text-2xl" />
+        )}
       </div>
     </div>
   );
