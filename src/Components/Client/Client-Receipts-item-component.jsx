616c2384fcf6f628d3702b5b5bc3b952f471c61c
RobJ <robjjohnsenwork@gmail.com>
2022-12-14 11:19:03 +0700

    Mobile - ClientActiveReceipts changes

--- src/Components/Client/Client-Receipts-item-component.jsx
+++ src/Components/Client/Client-Receipts-item-component.jsx
@@ -18,8 +18,12 @@ function ReceiptItem({ receipt }) {
         amount ? "bg-blue-200" : "bg-yellow-200"
       } rounded-xl text-center border-white border-2 hover:border-black hover:cursor-pointer`}
     >
-      <div onClick={toggleNote} className={`w-full flex gap-2 text-xl`}>
-        <span className="w-1/3 ">{date}</span>
+      <div
+        onClick={toggleNote}
+        className={`w-full flex gap-2 text-xl smlr:text-lg`}
+      >
+        <span className="w-1/3 smlst:hidden">{date}</span>
+        <span className="w-1/3 hidden smlst:block">{date.slice(5)}</span>
         <span className="w-1/3 ">{amount ? amount : "N/A"}</span>
         <span className="w-1/3 ">{sessions}</span>
       </div>
