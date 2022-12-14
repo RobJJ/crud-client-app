f2b443f4eb7c8d3a8ebfc6e36bcc9fbf4fefb693
RobJ <robjjohnsenwork@gmail.com>
2022-12-14 11:07:54 +0700

    Mobile - ClientPage, name display

--- src/SubPages/Home-client.jsx
+++ src/SubPages/Home-client.jsx
@@ -16,11 +16,11 @@ function HomeClient() {
   }
   //
   return (
-    <div className="w-full h-full bg-blue-800 p-5 pt-10 flex flex-col overflow-auto">
+    <div className="w-full h-full bg-blue-800 p-5 smlr:p-0 smlr:pt-5 smlr:pb-2 pt-10 flex flex-col overflow-auto">
       <div className="bg-blue-800 border-4 border-white h-full w-full rounded-xl flex flex-col ">
         {/* header - client name offset */}
         <div className="flex w-full justify-center">
-          <h2 className="bg-blue-800 font-muktaTitle font-bold border-4 border-white text-white text-center underline w-1/2 rounded-2xl  text-xl tracking-widest p-2 relative bottom-6">
+          <h2 className="bg-blue-800 font-muktaTitle font-bold border-4 border-white text-white text-center underline w-1/2 smlr:w-2/3 rounded-2xl  text-xl tracking-widest p-2 relative bottom-6">
             {focusedClient.name.toUpperCase()}
             <span ref={clientPageRef} className="absolute hidden right-5 top-2">
               <FaCheck className="text-green-500 text-3xl " />
