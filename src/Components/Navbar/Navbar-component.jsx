32005a1b3d592167aa4fd0e50114437a24fb7dcb
RobJ <robjjohnsenwork@gmail.com>
2022-12-14 10:31:55 +0700

    Mobile - NavBar changes

--- src/Components/Navbar/Navbar-component.jsx
+++ src/Components/Navbar/Navbar-component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../Context-Reducer/Context";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaSignOutAlt } from "react-icons/fa";
 import { ReactComponent as Spinner } from "../LoadingIcon/Bars-0.8s-234px.svg";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -31,9 +31,10 @@ function Navbar(params) {
     <div className="bg-blue-800 flex px-5 py-2 text-center font-mainNavBar tracking-widest">
       <Link
         onClick={handleSignOut}
-        className="bg-blue-800 w-1/3 p-2 underline text-2xl text-white"
+        className="bg-blue-800 w-1/3 p-2 underline text-2xl text-white flex justify-center items-center"
       >
-        SIGN OUT
+        <span className="smlst:hidden">SIGN OUT</span>
+        <FaSignOutAlt className="hidden smlst:block smlst:underline" />
       </Link>
 
       <Link
@@ -41,16 +42,27 @@ function Navbar(params) {
         onClick={handleHomeNavigation}
         className="bg-blue-800 w-1/3 p-2 underline text-2xl flex items-center justify-center gap-2 text-white"
       >
-        <FaHome />
-        <div>HOME</div>
+        <span>
+          <FaHome />
+        </span>
+        <div className="smlst:hidden">HOME</div>
       </Link>
 
       <div className="bg-blue-800 w-1/3 p-2 text-white underline text-2xl flex justify-center items-center">
-        {userInfo.displayName ? (
-          `${userInfo.displayName.toUpperCase()}`
-        ) : (
-          <Spinner className="w-20 p-0 m-0 text-2xl" />
-        )}
+        <span className="smlr:hidden">
+          {userInfo.displayName ? (
+            `${userInfo.displayName.toUpperCase()}`
+          ) : (
+            <Spinner className="w-20 p-0 m-0 text-2xl" />
+          )}
+        </span>
+        <span className="hidden smlr:block">
+          {userInfo.displayName ? (
+            `${userInfo.displayName.toUpperCase().split(" ")[0]}`
+          ) : (
+            <Spinner className="w-20 p-0 m-0 text-2xl" />
+          )}
+        </span>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      smlr: { max: "520px" },
+      smlst: { max: "400px" },
+    },
     fontFamily: {
       mainNavBar: ["Passion One", "cursive"],
       muktaTitle: ["Mukta", "sans-serif"],
