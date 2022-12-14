38f63faecb5572880a33d63931cda62af892dd76
RobJ <robjjohnsenwork@gmail.com>
2022-12-14 11:40:25 +0700

    Mobile - landingPage tweeks

--- src/Components/Welcome/Welcome-component.jsx
+++ src/Components/Welcome/Welcome-component.jsx
@@ -5,7 +5,7 @@ function Welcome(params) {
   //
   return (
     <div className="w-full h-3/5 p-2 bg-blue-500 border-4 border-white rounded-t-xl flex flex-col gap-5 items-center text-center overflow-auto">
-      <h2 className="text-white mt-2 font-mainNavBar text-4xl tracking-widest underline">
+      <h2 className="text-white mt-2 font-mainNavBar text-4xl smlr:text-3xl tracking-widest underline">
         Private client tracker
       </h2>
       <p className="text-white font-chivoReg w-full h-full p-2 overflow-auto">
--- src/Pages/Landing-page.jsx
+++ src/Pages/Landing-page.jsx
@@ -6,8 +6,8 @@ function LandingPage(params) {
   //
   //
   return (
-    <div className=" max-w-screen-md w-screen h-screen m-auto bg-blue-800 flex justify-center items-center">
-      <div className="w-2/3 h-2/3">
+    <div className=" max-w-screen-md  w-screen h-screen m-auto bg-blue-800 flex justify-center items-center">
+      <div className="w-2/3 h-2/3 med:w-5/6">
         <Toaster />
         <Welcome />
         <SignIn />
